fca6c4fc354caf68a8d4880af67bf7ed6b8e3be7
velicki <velicki22@gmail.com>
2023-06-01 01:52:01 +0200

    Optimized Code

--- Web_Scrape_Kursna_lista.py
+++ Web_Scrape_Kursna_lista.py
@@ -1,26 +1,8 @@
 import requests
-import pandas as pd
 from tkinter import *
 from bs4 import BeautifulSoup
-from tkhtmlview import HTMLLabel
+import pandas as pd
 
-def fetch_data():
-    try:
-        response = requests.get('https://www.kursna-lista.info/')
-        response.raise_for_status()  # Raise exception for unsuccessful HTTP response
-        html = response.text
-        soup = BeautifulSoup(html, 'lxml')
-        table = soup.find('table', id='kursna-lista')
-        rows = table.find_all('tr')[1:]
-
-        headers = ['Šifra Valute', 'Zemlja', 'Valuta', 'Količina', 'Kupovni Kurs', 'Srednji Kurs', 'Prodajni Kurs']
-        data = [[td.text for td in row.find_all('td')[1:8]] for row in rows[:6]]
-
-        df = pd.DataFrame(data, columns=headers)
-        return df
-    except (requests.RequestException, ValueError) as e:
-        print(f"Error fetching data: {e}")
-        return None
 
 def windows_configuration():
     K_Lista.geometry("530x250")
@@ -30,36 +12,58 @@ def windows_configuration():
     K_Lista.iconphoto(True, icon)
     K_Lista.config(bg="white")
 
-def headline():
-    headline_text = fetch_data()
-    if headline_text is not None:
-        headline = Label(K_Lista, text=headline_text, font=("Arial", 10), fg="BLACK", borderwidth=1, relief="solid")
-        return headline
-    else:
+
+def fetch_data():
+    try:
+        response = requests.get('https://www.kursna-lista.info/')
+        response.raise_for_status()
+        html = response.text
+        soup = BeautifulSoup(html, 'lxml')
+        return soup
+    except (requests.RequestException, ValueError) as e:
+        print(f"Error fetching data: {e}")
         return None
+
+
+def get_headline_text():
+    date1 = fetch_data().find('div', id='maint-table-header2')
+    for i in date1.find_all('h2'):
+        headline_text = i.text.strip()
+    return headline_text
+
+
+def get_rows_data():
+    table1 = fetch_data().find('table', id='kursna-lista')
+    rows = table1.find_all('tr')[1:]
+    data = [[td.text for td in row.find_all('td')[1:8]] for row in rows]
+    return data
+
 
 def display_data():
-    data = fetch_data()
-    if data is not None:
-        df = pd.DataFrame(data)
-        styled_df = df.style.set_table_styles([{'selector': 'tr', 'props': [('border', 'solid 1px')]}, {'selector': 'th', 'props': [('border', 'solid 1px')]}, {'selector': 'td', 'props': [('border', 'solid 1px')]}]).set_table_attributes("border=1").set_properties(**{'border-collapse': 'collapse', 'border': 'solid'})
-        html_table = styled_df.to_html(index=False).replace('<table', '<table style="border-collapse:collapse;border:solid"')
-        maintext = HTMLLabel(K_Lista, html=html_table)
-        maintext.grid(row=2, column=0, columnspan=7, pady=2, padx=2)
-    else:
-        return None
+    headline_text = get_headline_text()
+    headline_label = ["Šifra Valute", "Zemlja", "Valuta", "Količina", "Kupovni Kurs", "Srednji Kurs", "Prodajni Kurs"]
+
+    headline = Label(K_Lista, text=headline_text, font=("Arial", 10), fg="BLACK", borderwidth=1, relief="solid")
+    headline.grid(row=0, column=0, pady=15, padx=2, columnspan=7)
+
+    for i, label in enumerate(headline_label):
+        field1 = Label(K_Lista, text=label, font=("Arial", 10), fg="BLACK", borderwidth=1, relief="solid")
+        field1.grid(row=1, column=i, pady=2, padx=2)
+
+    for i in range(0, 6):
+        for j in range(0, 7):
+            field = Label(K_Lista, text=get_rows_data()[i][j], font=("Arial", 10), fg="BLACK", borderwidth=1,
+                          relief="solid")
+            field.grid(row=i + 2, column=j, pady=2, padx=2)
+
+
+# ------------------------------------------
 
 K_Lista = Tk()
+
+canvas = Canvas(K_Lista, width=530, height=250, bg="gray")
+
 windows_configuration()
-
-headline_label = headline()
-if headline_label is not None:
-    headline_label.grid(row=0, column=0, pady=2, padx=2, columnspan=7)
-
-labels = ['Šifra Valute', 'Zemlja', 'Valuta', 'Količina', 'Kupovni Kurs', 'Srednji Kurs', 'Prodajni Kurs']
-for i, label in enumerate(labels):
-    lbl = Label(K_Lista, text=label, font=("Arial", 10), fg="BLACK", borderwidth=1, relief="solid")
-    lbl.grid(row=1, column=i, pady=2, padx=2)
 
 display_data()
 
